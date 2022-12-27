32ffbc586aeddc998d74bc8bb8fdcbbbb4d877c4
Michael Vo <dev@bumbleparrot.com>
2022-12-27 17:39:26 -0500

    Refactored tasks controller to use more Elixiry syntax with pipes.

--- lib/hnydew_api_web/controllers/task_controller.ex
+++ lib/hnydew_api_web/controllers/task_controller.ex
@@ -6,7 +6,9 @@ defmodule HnydewApiWeb.TaskController do
 
   def index(conn, _params) do
     tasks = Tasks.list_tasks()
-    render(conn, "index.json", tasks: tasks)
+
+    conn
+    |> render("index.json", tasks: tasks)
   end
 
   def create(conn, %{"task" => task_params}) do
@@ -22,18 +24,13 @@ defmodule HnydewApiWeb.TaskController do
         |> put_view(HnydewApiWeb.ErrorView)
         |> render("index.json", errors: errors)
     end
-
-    # with {:ok, %Task{} = task} <- Tasks.create_task(task_params) do
-    #   conn
-    #   |> put_status(:created)
-    #   |> put_resp_header("location", Routes.task_path(conn, :show, task))
-    #   |> render("show.json", task: task)
-    # end
   end
 
   def show(conn, %{"id" => id}) do
     task = Tasks.get_task(id)
-    render(conn, "show.json", task: task)
+
+    conn
+    |> render("show.json", task: task)
   end
 
   def get_tasks_by_user_id(conn, %{"user_id" => user_id}) do
